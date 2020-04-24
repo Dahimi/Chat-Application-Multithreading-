682c7c534b41a50cd766d005307a61f074ac7b9f
DahimiSoufiane <DAHIMI Soufiane>
2020-04-24 11:24:21 +0000

    now we start implementing the server side

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,52 @@
 package server ;
+import java.net.*;
+import java.util.Map;
+import java.io.*;
+import java.util.concurrent.ConcurrentHashMap;
 public class Server {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
+	private static Map<String, Connection> connectionMap = new ConcurrentHashMap<String, Connection>();
+    public static void sendBroadcastMessage(Message message) {
+    	for(Map.Entry<String, Connection>  pair : connectionMap.entrySet()) {
+    		try {
+    		((Connection) pair.getValue()).send(message);
+    		} catch(IOException e ) {
+    			ConsoleHelper.writeMessage("The message couldn't be sent ");
+    		}
+    	}
+    }
+	
+    	public static void main(String[] args){
+        ServerSocket serverSocket = null ;
+        try{
+        int port  = ConsoleHelper.readInt();
+        serverSocket = new ServerSocket(port);
+        ConsoleHelper.writeMessage("The server is running ");
+        while(true){
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket); 
+                handler.start();
+            }
+        } catch(Exception e){
+            try{
+            if( serverSocket != null) serverSocket.close();
+            }
+            catch(IOException ex){
+                
+            }
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+        
+    }
+    private static class Handler extends Thread {
+        private final Socket socket ;
+        public Handler(Socket  socket){
+            this.socket = socket;
+            
+        }
+        @Override
+        public void run(){
+            
+        }
+    }
+    
 }
